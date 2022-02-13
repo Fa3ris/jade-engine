7c71f598b015ccc72f025d7fa6064eaef6f2e42e
FabriceT <fabrice.tsiava@artik-consulting.com>
2022-02-13 16:48:34 +0100

    center window

--- src/main/java/org/jade/Window.java
+++ src/main/java/org/jade/Window.java
@@ -11,10 +11,14 @@ import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
 import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
 import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
@@ -23,11 +27,15 @@ import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
+import java.nio.IntBuffer;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,7 +88,7 @@ public class Window {
     glfwDefaultWindowHints(); // optional, the current window hints are already the default
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE); // full window
+    glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE); // full window
 
     if (faulty) {
       // request an impossible version of openGL
@@ -88,12 +96,37 @@ public class Window {
       glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     }
 
+    // Get the resolution of the primary monitor
+    GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+    if (vidmode == null) {
+      throw new IllegalStateException("Unable to get vidmode");
+    }
+
+    w = (int) (vidmode.width() * .5);
+    h = (int) (vidmode.height() * .5);
     // create window
     windowHandle = glfwCreateWindow(w, h, title, NULL, NULL);
 
 
     if ( windowHandle == NULL )
       throw new RuntimeException("Failed to create the GLFW window");
+
+    // Get the thread stack and push a new frame
+    try ( MemoryStack stack = stackPush() ) {
+      IntBuffer pWidth = stack.mallocInt(1); // int*
+      IntBuffer pHeight = stack.mallocInt(1); // int*
+
+      // Get the window size passed to glfwCreateWindow
+      glfwGetWindowSize(windowHandle, pWidth, pHeight);
+
+      // Center the window
+      glfwSetWindowPos(
+          windowHandle,
+          (int) ((vidmode.width() - pWidth.get(0)) * .5),
+          (int) ((vidmode.height() - pHeight.get(0)) * .5)
+      );
+    } // the stack frame is popped automatically
 
 
     // Make the OpenGL the current context for the window
