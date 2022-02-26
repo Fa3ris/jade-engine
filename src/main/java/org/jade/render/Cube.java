8385bede59e50582ee5faa8c7ce7a69b3c8cd2a0
FabriceT <fabrice.tsiava@artik-consulting.com>
2022-02-26 21:40:46 +0100

    split transform uniform in three parts

--- src/main/java/org/jade/render/Cube.java
+++ src/main/java/org/jade/render/Cube.java
@@ -25,8 +25,6 @@ public class Cube {
   private final Matrix4f rotation = new Matrix4f();
   private final Vector3f rotationAxis = new Vector3f(.5f, 1f, 0f);
 
-  private final Matrix4f projection = new Matrix4f();
-
   private Vector3f translation;
   private int rotationOffset;
 
@@ -78,8 +76,8 @@ public class Cube {
     };
 
     triangles = new Triangles(vertices, 0, 36,
-        "shaders/texture/vertexShader.glsl",
-        "shaders/texture/fragmentShader.glsl");
+        "shaders/cube/vertexShader.glsl",
+        "shaders/cube/fragmentShader.glsl");
 
     triangles.configVertexAttribute(0, 3, 5*Float.BYTES, 0);
     triangles.configVertexAttribute(2, 2, 5*Float.BYTES, 3*Float.BYTES);
@@ -94,19 +92,21 @@ public class Cube {
     smileyTexture = new Texture("textures/awesomeface.png", GL_RGBA);
     smileyTexture.load(true);
 
-    // static too
     Vector3f moveSceneForward = new Vector3f(0f, 0f, -3f);
-    Matrix4f view = new Matrix4f();
-    view.translate(moveSceneForward); // move the entire scene forward == move camera backwards
+    Matrix4f view = new Matrix4f()
+        .translate(moveSceneForward); // move the entire scene forward == move camera backwards
 
-    projection.perspective(
+    triangles.shader.setUniformMatrix4fv("view", view);
+
+    Matrix4f projection = new Matrix4f()
+        .perspective(
         (float) Math.toRadians(45f), // field of view
         800f/600f, // aspect ratio
         0.1f, // near plane
         100f, // far plane
         false); // z axis in range 0:1 of -1:1
 
-    projection.mul(view); // result is stored in projection
+    triangles.shader.setUniformMatrix4fv("projection", projection);
   }
 
 
@@ -127,7 +127,7 @@ public class Cube {
 
     rotation.rotate((float) (glfwGetTime() * Math.toRadians(50f) + rotationOffset), rotationAxis);
 
-    triangles.shader.setUniformMatrix4fv("transform", new Matrix4f(projection).mul(rotation));
+    triangles.shader.setUniformMatrix4fv("model", rotation);
 
     wallTexture.use(GL_TEXTURE0);
     smileyTexture.use(GL_TEXTURE1);
